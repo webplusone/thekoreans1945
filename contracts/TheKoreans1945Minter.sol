7d3efaa61b69223ddc0eb354b4cd57a628d49575
TheGreatHB <hb@thegreathb.com>
2022-08-12 23:57:33 +0900

    Add user whitelist

--- contracts/TheKoreans1945Minter.sol
+++ contracts/TheKoreans1945Minter.sol
@@ -5,12 +5,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./standards/IERC721G.sol";
 
 contract TheKoreans1945Minter is Ownable {
-    event SetNFTs(address nft, bool status);
-    event Mint(address user, uint256 tokenId, address nft, uint256 mintedAmount);
+    event SetNFTWhitelist(address nft, bool status);
+    event SetUserWhitelist(address user, bool status);
+    event Mint(address user, uint256 tokenId);
     event Airdrop(address[] users, uint256 initialId, uint256 lastId);
 
     IERC721G public immutable KOREANS;
     mapping(address => bool) public isListedNFT;
+    mapping(address => bool) public isListedUser;
     mapping(address => mapping(address => uint256)) public mintedAmount;
 
     constructor(IERC721G _KOREANS, address[] memory nfts) {
@@ -19,14 +21,21 @@ contract TheKoreans1945Minter is Ownable {
         uint256 amount = nfts.length;
         for (uint256 i = 0; i < amount; i++) {
             isListedNFT[nfts[i]] = true;
-            emit SetNFTs(nfts[i], true);
+            emit SetNFTWhitelist(nfts[i], true);
         }
     }
 
-    function setNFTs(address[] calldata nfts, bool status) external onlyOwner {
+    function setNFTWhitelist(address[] calldata nfts, bool status) external onlyOwner {
         for (uint256 i = 0; i < nfts.length; i++) {
             isListedNFT[nfts[i]] = status;
-            emit SetNFTs(nfts[i], status);
+            emit SetNFTWhitelist(nfts[i], status);
+        }
+    }
+
+    function setUserWhitelist(address[] calldata users, bool status) external onlyOwner {
+        for (uint256 i = 0; i < users.length; i++) {
+            isListedUser[users[i]] = status;
+            emit SetUserWhitelist(users[i], status);
         }
     }
 
@@ -34,20 +43,26 @@ contract TheKoreans1945Minter is Ownable {
         uint256 startId = KOREANS.totalSupply();
         for (uint256 i = 0; i < users.length; i++) {
             KOREANS.mint(users[i], startId + i);
+            emit Mint(users[i], startId + i);
         }
         emit Airdrop(users, startId, startId + users.length - 1);
     }
 
-    function mint(address nft) external {
+    function mintByNFTWhitelist(address nft) external {
         require(isListedNFT[nft], "INVALID_NFT_ADDRESS");
-        uint256 balance = IERC721(nft).balanceOf(msg.sender);
-        uint256 _mintedAmount = mintedAmount[msg.sender][nft];
-        require(_mintedAmount < balance, "OUT_OF_RANGE");
+        require(mintedAmount[msg.sender][nft]++ < IERC721(nft).balanceOf(msg.sender), "OUT_OF_RANGE");
+        _mint();
+    }
 
+    function mintByUserWhitelist() external {
+        require(isListedUser[msg.sender], "UNAUTHORIZED");
+        delete isListedUser[msg.sender];
+        _mint();
+    }
+
+    function _mint() internal {
         uint256 tokenId = KOREANS.totalSupply();
         KOREANS.mint(msg.sender, tokenId);
-        mintedAmount[msg.sender][nft] = _mintedAmount + 1;
-
-        emit Mint(msg.sender, tokenId, nft, _mintedAmount + 1);
+        emit Mint(msg.sender, tokenId);
     }
 }
