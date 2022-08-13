7e7421c850423d6f74fbc0dc4f0509eba905bc98
TheGreatHB <hb@thegreathb.com>
2022-08-13 18:24:15 +0900

    Separate Minting by time.stamp

--- contracts/TheKoreans1945Minter.sol
+++ contracts/TheKoreans1945Minter.sol
@@ -10,18 +10,35 @@ contract TheKoreans1945Minter is Ownable {
     event Mint(address user, uint256 tokenId);
     event Airdrop(address[] users, uint256 initialId, uint256 lastId);
 
+    uint256 public constant preMintTime = 1660564800; //15Aug22 12:00PM UTC
+    uint256 public constant wlMintTime = 1660568400; //15Aug22 13:00PM UTC
+    uint256 public constant publicMintTime = 1660654800; //16Aug22 13:00PM UTC
+
     IERC721G public immutable KOREANS;
+    mapping(address => bool) public nftForPreMint;
     mapping(address => bool) public isListedNFT;
     mapping(address => bool) public isListedUser;
     mapping(address => mapping(address => uint256)) public mintedAmount;
 
-    constructor(IERC721G _KOREANS, address[] memory nfts) {
+    constructor(IERC721G _KOREANS, address[] memory wlNFTs, address[] memory nftsForPreMint) {
         KOREANS = _KOREANS;
 
-        uint256 amount = nfts.length;
+        uint256 amount = wlNFTs.length;
         for (uint256 i = 0; i < amount; i++) {
-            isListedNFT[nfts[i]] = true;
-            emit SetNFTWhitelist(nfts[i], true);
+            isListedNFT[wlNFTs[i]] = true;
+            emit SetNFTWhitelist(wlNFTs[i], true);
+        }
+
+        amount = nftsForPreMint.length;
+        require(amount == 4, "INVALID_NFTS_FOR_PREMINT");
+        for (uint256 i = 0; i < amount; i++) {
+            nftForPreMint[nftsForPreMint[i]] = true;
+        }
+    }
+
+    function setNFTForPreMint(address[] calldata nfts, bool status) external onlyOwner {
+        for (uint256 i = 0; i < nfts.length; i++) {
+            nftForPreMint[nfts[i]] = status;
         }
     }
 
@@ -49,12 +66,27 @@ contract TheKoreans1945Minter is Ownable {
     }
 
     function mint(address nft) external {
-        if (nft == address(0)) {
-            require(isListedUser[msg.sender], "UNAUTHORIZED");
-            delete isListedUser[msg.sender];
-        } else {
-            require(isListedNFT[nft], "INVALID_NFT_ADDRESS");
-            require(mintedAmount[msg.sender][nft]++ < IERC721(nft).balanceOf(msg.sender), "OUT_OF_RANGE");
+        if (block.timestamp < preMintTime) {
+            revert("NOT_STARTED_YET");
+        } else if (block.timestamp < wlMintTime) {
+            if (nft == address(0)) {
+                require(isListedUser[msg.sender], "UNAUTHORIZED");
+                delete isListedUser[msg.sender];
+            } else {
+                require(nftForPreMint[nft], "NOT_YET_WITH_THIS_NFT");
+                require(isListedNFT[nft], "INVALID_NFT_ADDRESS");
+                uint256 _mintedAmount = mintedAmount[msg.sender][nft]++;
+                require(_mintedAmount == 0, "ONLY_1_NFT_IN_PREMINT_TIME");
+                require(_mintedAmount < IERC721(nft).balanceOf(msg.sender), "OUT_OF_RANGE");
+            }
+        } else if (block.timestamp < publicMintTime) {
+            if (nft == address(0)) {
+                require(isListedUser[msg.sender], "UNAUTHORIZED");
+                delete isListedUser[msg.sender];
+            } else {
+                require(isListedNFT[nft], "INVALID_NFT_ADDRESS");
+                require(mintedAmount[msg.sender][nft]++ < IERC721(nft).balanceOf(msg.sender), "OUT_OF_RANGE");
+            }
         }
 
         uint256 tokenId = KOREANS.totalSupply();
